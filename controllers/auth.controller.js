3686b3e3800a1a75e552db6591743fd669f99d98
Luis Bermudez <lapbermudez@gmail.com>
2022-02-25 17:08:26 -0600

    done

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -75,13 +75,13 @@ exports.loginAuth = async (req, res) => {
               maxAge: 1000 * 60 * 60 * 24,
               httpOnly: true,
               sameSite: true,
-              secure: true
+              secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
             });
 
             res.cookie("signature", signature, {
               httpOnly: true,
               sameSite: true,
-              secure: true,
+              secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
             });
 
             res.status(200).json({ message: 'Credentials have been authenticated.' });
