e15277bae663f5d4a133bf9bade6ef98b248e5cc
Luis Bermudez <lapbermudez@gmail.com>
2022-02-25 16:14:44 -0600

    done

--- app.js
+++ app.js
@@ -22,4 +22,8 @@ app.use("/api", allRoutes);
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
+app.use((req, res, next) => {
+    res.sendFile(__dirname + "/public/build/index.html");
+});
+
 module.exports = app;
