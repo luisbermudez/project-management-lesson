415837ee2e43bf800b682cf9ab8becc016f35bfc
Luis Bermudez <lapbermudez@gmail.com>
2022-02-25 20:23:44 -0600

    done

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -74,13 +74,13 @@ exports.loginAuth = async (req, res) => {
             res.cookie("headload", `${header}.${payload}`, {
               maxAge: 1000 * 60 * 60 * 24,
               httpOnly: true,
-              sameSite: true,
+              sameSite: 'None',
               secure: true
             });
 
             res.cookie("signature", signature, {
               httpOnly: true,
-              sameSite: true,
+              sameSite: 'None',
               secure: true,
             });
 
